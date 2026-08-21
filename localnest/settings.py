8560a953af653f0a6b03a45b8377f1013b27e56c
jenish jethava <jethvajenish2@gmail.com>
2026-08-21 17:40:51 +0530

    fix: set absolute MEDIA_URL/STATIC_URL paths and enable media serving fallback in production

--- localnest/settings.py
+++ localnest/settings.py
@@ -127,12 +127,12 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media storage configuration
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Cloudinary Integration
